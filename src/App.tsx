a8b51f05f422d1375acffa55ed27ad6c203c71f0
gbpinheiro1 <gabrielpinheiro551@gmail.com>
2026-05-13 21:51:17 -0300

    style: heading style changes

--- src/App.tsx
+++ src/App.tsx
@@ -15,9 +15,9 @@ function Header() {
         <h1 className="text-3xl font-bold">Controle de Hábitos</h1>
         <span className="text-zinc-400 text-sm">1/1 concluído(s) hoje.</span>
       </div>
-      <div className="flex flex-col gap-1">
-        <span className="text-zinc-400 text-sm">10 de Maio - 16 de Maio</span>
-        <div className="flex items-center gap-3">
+      <div className="flex flex-col gap-1 items-end">
+        <span className="text-zinc-400 text-sm ">10/05 - 16/05</span>
+        <div className="flex items-center gap-3 justify-center">
           <Button text="Ant."></Button>
           <Button text="Próx."></Button>
         </div>
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,5 +3,9 @@ interface Props {
 }
 
 export function Button({ text }: Props) {
-  return <button className="bg-violet-600 hover:bg-violet-500">{text}</button>
+  return (
+    <button className="bg-violet-600 hover:bg-violet-500 transition-colors rounded px-2 py-1 disabled:opacity-30 disabled:cursor-not-allowed">
+      {text}
+    </button>
+  )
 }
